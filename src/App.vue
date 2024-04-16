d74d17331b26774ad7d0c5efd8d8f90f19fbff9b
lin.liu <1634060836@qq.com>
2024-04-17 00:00:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view /> <!-- 这里是用于显示当前路由对应的组件的地方 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* 设置所有元素的边距和内边距为零 */
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
